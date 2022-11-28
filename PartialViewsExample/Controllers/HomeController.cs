e36775b9ba89b440a328928e2253a2449053b3a0
Aman Ahmed <aman16.aa17@gmail.com>
2022-11-28 14:15:53 +0100

    Partial Views with ViewData

--- PartialViewsExample/Controllers/HomeController.cs
+++ PartialViewsExample/Controllers/HomeController.cs
@@ -7,6 +7,14 @@ namespace PartialViewsExample.Controllers
 		[Route("/")]
 		public IActionResult Index()
 		{
+			ViewData["ListTitle"] = "Cities";
+			ViewData["ListItems"] = new List<string>()
+			{
+				"Paris",
+				"New York",
+				"New Mumbai",
+				"Rome"
+			};
 			return View();
 		}
 
